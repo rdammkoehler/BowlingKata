b316ce55db7a5a83eff61f3e395b8a4a42c11fb2
Rich Dammkoehler <rpd@noradltd.com>
2010-10-12 23:12:33 -0500

    Rich D: Further modification of the score method to calculate the spare frame correctly

--- src/main/java/kata/Game.java
+++ src/main/java/kata/Game.java
@@ -11,8 +11,12 @@ public class Game {
 
 	public int score() {
 		int score = 0;
-		for(int roll : rolls) {
-			score += roll;
+		for(int frame = 0, ball = 0; frame < 10; frame++, ball += 2 ) {
+			if ( rolls[ball] + rolls[ball+1] == 10 ) { //spare
+				score += 10 + rolls[ball+2];
+			} else {
+				score += rolls[ball] + rolls[ball+1];
+			}
 		}
 		return score;
 	}
