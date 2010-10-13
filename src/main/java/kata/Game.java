ab4cccb45d738da8461e5352696185651941c288
Rich Dammkoehler <rpd@noradltd.com>
2010-10-12 23:07:06 -0500

    Rich D: More design change, use the tracked rolls to determine score by summation

--- src/main/java/kata/Game.java
+++ src/main/java/kata/Game.java
@@ -2,16 +2,18 @@ package kata;
 
 public class Game {
 
-	private int score = 0;
 	private int rolls[] = new int[21];
 	private int currentRoll = 0;
 	
 	public void roll(int pins) {
-		score += pins;	
 		rolls[currentRoll++] = pins;
 	}
 
 	public int score() {
+		int score = 0;
+		for(int roll : rolls) {
+			score += roll;
+		}
 		return score;
 	}
 
