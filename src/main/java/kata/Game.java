af97d9610e8e58faee875f380901150355b556d5
Rich Dammkoehler <rpd@noradltd.com>
2010-10-12 22:47:27 -0500

    Rich D: Step 2, create the first test and make it pass

---
+++ src/main/java/kata/Game.java
@@ -0,0 +1,13 @@
+package kata;
+
+public class Game {
+
+	public void roll(int pins) {
+		
+	}
+
+	public int score() {
+		return 0;
+	}
+
+}
--- src/test/java/kata/BowlingGameTest.java
+++ src/test/java/kata/BowlingGameTest.java
@@ -1,7 +1,16 @@
 package kata;
 
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.*;
 
 public class BowlingGameTest {
 
+	@Test(groups={"fast"})
+	public void shouldScoreZeroOnGutterGame() {
+		Game game = new Game();
+		for(int i = 0; i < 20; i++ ) {
+			game.roll(0);
+		}
+		assertEquals(game.score(), 0);
+	}
 }
